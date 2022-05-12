c54a805c14e27b0ee3ccb289eb3dd0804fed6e99
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-05-13 00:11:23 +0200

    Created Base interface

---
+++ src/main/java/mafiadelprimobanco/focusproject/BaseController.java
@@ -0,0 +1,44 @@
+package mafiadelprimobanco.focusproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class BaseController {
+	
+	@FXML
+	void onNavHomeClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	@FXML
+	void onNavProgressClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	@FXML
+	void onNavStatisticsClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	@FXML
+	void onNavTagClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	@FXML
+	void onNavInfoClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	@FXML
+	void onNavUserClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	@FXML
+	void onNavSettingsClick(ActionEvent ignoredEvent) {
+	
+	}
+	
+	
+}
