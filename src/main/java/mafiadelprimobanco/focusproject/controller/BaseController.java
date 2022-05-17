36e67709e57611e5b88802968536386893135716
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-05-17 12:02:35 +0200

    In Base switched from button to toggleButton
    also we are now using a stack-pane as content root

--- src/main/java/mafiadelprimobanco/focusproject/controller/BaseController.java
+++ src/main/java/mafiadelprimobanco/focusproject/controller/BaseController.java
@@ -2,61 +2,86 @@ package mafiadelprimobanco.focusproject.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import mafiadelprimobanco.focusproject.SceneHandler;
 import mafiadelprimobanco.focusproject.model.utils.FXMLReferences;
-
-import java.io.IOException;
 
 public class BaseController
 {
 	@FXML
-	private BorderPane baseBorderPane;
+	private BorderPane borderPane;
 
 	@FXML
-	void onNavHomeClick(ActionEvent ignoredEvent)
+	private StackPane contentRoot;
+
+	@FXML
+	void initialize()
+	{
+		SceneHandler.getInstance().setContentRoot(contentRoot);
+	}
+
+
+	@FXML
+	void onNavHomeClick(ActionEvent event)
 	{
 		navTo(FXMLReferences.HOME, "Home");
+		if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	@FXML
-	void onNavProgressClick(ActionEvent ignoredEvent)
+	void onNavProgressClick(ActionEvent event)
 	{
 		navTo(FXMLReferences.PROGRESS, "Progressi");
+		if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	@FXML
-	void onNavStatisticsClick(ActionEvent ignoredEvent)
+	void onNavStatisticsClick(ActionEvent event)
 	{
 		navTo(FXMLReferences.STATISTICS, "Statistiche");
+			if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	@FXML
-	void onNavTagClick(ActionEvent ignoredEvent)
+	void onNavTagClick(ActionEvent event)
 	{
 		navTo(FXMLReferences.TAGS, "Tag");
+			if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	@FXML
-	void onNavInfoClick(ActionEvent ignoredEvent)
+	void onNavInfoClick(ActionEvent event)
 	{
 		// todo: implement
+			if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	@FXML
-	void onNavUserClick(ActionEvent ignoredEvent)
+	void onNavUserClick(ActionEvent event)
 	{
 		// todo: implement
+			if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	@FXML
-	void onNavSettingsClick(ActionEvent ignoredEvent)
+	void onNavSettingsClick(ActionEvent event)
 	{
 		// todo: implement
+		if(event.getSource() instanceof ToggleButton toggleButton)
+			toggleButton.setSelected(true);
 	}
 
 	private void navTo(String pagePathRef, String pageName)
 	{
+		/* disabled for now
 		try
 		{
 			SceneHandler.getInstance().loadPage(pagePathRef);
@@ -67,12 +92,9 @@ public class BaseController
 			SceneHandler.getInstance().showErrorMessage("Errore Di caricamento",
 					"Impossibile caricare la pagina " + pageName);
 		}
+		*/
 	}
 
-	@FXML
-	void initialize()
-	{
-		SceneHandler.getInstance().setBaseBorderPane(baseBorderPane);
-	}
+
 
 }
