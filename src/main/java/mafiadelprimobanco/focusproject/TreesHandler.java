28b2b0a7e8b58b9f09611719aa181eb22d9596e1
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-05-29 13:05:36 +0200

    Initial trees implementation

---
+++ src/main/java/mafiadelprimobanco/focusproject/TreesHandler.java
@@ -0,0 +1,44 @@
+package mafiadelprimobanco.focusproject;
+
+import mafiadelprimobanco.focusproject.model.TreeTemplate;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class TreesHandler
+{
+	private static final TreesHandler instance = new TreesHandler();
+
+	public static TreesHandler getInstance()
+	{
+		return instance;
+	}
+
+	private HashMap<Integer, TreeTemplate> trees = new HashMap<>();
+
+	public TreesHandler()
+	{
+		createTrees();
+	}
+
+	private void createTrees()
+	{
+		// TESTING ONLY
+		String deadTree = "trees/testingDeadTree.png";
+		trees.put(0, new TreeTemplate(0, "albero 1", "trees/tile000.png", deadTree));
+		trees.put(1, new TreeTemplate(1, "albero 2", "trees/tile001.png", deadTree));
+		trees.put(2, new TreeTemplate(2, "albero 3", "trees/tile007.png", deadTree));
+		trees.put(3, new TreeTemplate(3, "albero 4", "trees/tile008.png", deadTree));
+	}
+
+	public Collection<TreeTemplate> getTrees()
+	{
+		return trees.values();
+	}
+
+	public TreeTemplate getTree(int uuid)
+	{
+		return trees.get(uuid);
+	}
+
+}
---
+++ src/main/java/mafiadelprimobanco/focusproject/model/TreeTemplate.java
@@ -0,0 +1,18 @@
+package mafiadelprimobanco.focusproject.model;
+
+import javafx.scene.image.Image;
+
+public record TreeTemplate(Integer uuid, String name, String matureTreeSprite, String deadTreeSprite)
+{
+
+	public Image createMatureTreeImage()
+	{
+		return new Image(matureTreeSprite);
+	}
+
+	public Image createDeadTreeImage()
+	{
+		return new Image(deadTreeSprite);
+	}
+
+}
