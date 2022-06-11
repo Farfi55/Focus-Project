5a73bd6bdc3516366446c94b455deecfce5d6566
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-06-11 12:51:35 +0200

    Barebones Progress Interface

--- src/main/java/mafiadelprimobanco/focusproject/controller/ProgressPageController.java
+++ src/main/java/mafiadelprimobanco/focusproject/controller/ProgressPageController.java
@@ -1,26 +1,47 @@
 package mafiadelprimobanco.focusproject.controller;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import mafiadelprimobanco.focusproject.Localization;
-import mafiadelprimobanco.focusproject.controller.Controller;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProgressPageController implements Controller
 {
-	@FXML private Label pageTitle;
+
+	@FXML private MFXComboBox<String> intervalComboBox;
+
+	@FXML private GridPane toUnlockTreeDetailsGrid;
+
+	@FXML private GridPane treeGrid;
+
+	@FXML private HBox treeSelectionHBox;
+
+	@FXML private GridPane unlockedTreeDetailsGrid;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
-		Localization.setLabel(pageTitle, "progressPage.name");
-	}
 
+	}
 
 	@Override
 	public void terminate()
+	{
+
+	}
+
+	@FXML
+	void onTreeSelectNextAction(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void onTreeSelectPreviousAction(ActionEvent event)
 	{
 
 	}
