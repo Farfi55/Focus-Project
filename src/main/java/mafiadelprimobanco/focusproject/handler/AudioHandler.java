408c171dc7815e25d2184709b5602147d3b9efdf
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2022-06-18 20:02:33 +0200

    Added play sound methods

--- src/main/java/mafiadelprimobanco/focusproject/handler/AudioHandler.java
+++ src/main/java/mafiadelprimobanco/focusproject/handler/AudioHandler.java
@@ -1,6 +1,7 @@
 package mafiadelprimobanco.focusproject.handler;
 
-import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.*;
+import mafiadelprimobanco.focusproject.utils.ResourcesLoader;
 
 
 
@@ -8,7 +9,12 @@ public class AudioHandler
 {
 	private static final AudioHandler instance = new AudioHandler();
 
-	private SettingsHandler settingsHandler;
+	private MediaPlayer mediaPlayer;
+	private Media toggleButtonAudioClip;
+	private Media buttonAudioClip;
+	private Media notificationAudioClip;
+	private Media activitySuccessAudioClip;
+	private final SettingsHandler settingsHandler;
 
 	public static AudioHandler getInstance()
 	{
@@ -18,5 +24,54 @@ public class AudioHandler
 	AudioHandler()
 	{
 		this.settingsHandler = SettingsHandler.getInstance();
+		loadAudioClips();
 	}
+
+	private void loadAudioClips()
+	{
+		toggleButtonAudioClip = new Media(ResourcesLoader.load("audio/click1.wav"));
+		buttonAudioClip = new Media(ResourcesLoader.load("audio/click5.wav"));
+		notificationAudioClip = new Media(ResourcesLoader.load("audio/rollover3.wav"));
+		activitySuccessAudioClip = new Media(ResourcesLoader.load("audio/activitySuccess.wav"));
+	}
+
+	public void playToggleButtonAudioClip()
+	{
+		mediaPlayer = new MediaPlayer(toggleButtonAudioClip);
+		useGlobalVolume();
+		mediaPlayer.seek(toggleButtonAudioClip.getDuration());
+		mediaPlayer.play();
+	}
+
+	public void playButtonAudioClip()
+	{
+		mediaPlayer = new MediaPlayer(buttonAudioClip);
+		useGlobalVolume();
+		mediaPlayer.seek(buttonAudioClip.getDuration());
+		mediaPlayer.play();
+	}
+
+	public void playNotificationAudioClip()
+	{
+		mediaPlayer = new MediaPlayer(notificationAudioClip);
+		useGlobalVolume();
+		mediaPlayer.seek(notificationAudioClip.getDuration());
+		mediaPlayer.play();
+	}
+
+	public void playActivitySuccessAudioClip()
+	{
+		mediaPlayer = new MediaPlayer(activitySuccessAudioClip);
+		useGlobalVolume();
+		mediaPlayer.seek(activitySuccessAudioClip.getDuration());
+		mediaPlayer.play();
+	}
+
+	void useGlobalVolume()
+	{
+		mediaPlayer.setVolume(settingsHandler.getSettings().soundVolume.get() / 100.0);
+	}
+
+
+
 }
